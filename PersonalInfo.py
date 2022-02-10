5b3498121293413f2b128807a29158183af7aa7c
Cratertater <92005481+Cratertater@users.noreply.github.com>
2022-02-10 11:42:37 -0600

    Delete PersonalInfo.py

--- PersonalInfo.py
+++
@@ -1,8 +0,0 @@
-#Name
-print("Jake Million")
-#Address
-print("Galway Dr Mathis, Texas 78368")
-#Number
-print("619 - 820- 5418")
-#Major
-print("Computer Science with a concentration in Cyber Security")
