54e480b0aa756fb1a7cdc3a097a012791327824a
Cratertater <92005481+Cratertater@users.noreply.github.com>
2022-02-10 11:40:44 -0600

    Delete LeapYear.py

--- LeapYear.py
+++
@@ -1,9 +0,0 @@
-#Getting a year from user
-y = int(input("Enter a year: "))
-#leap year check
-if y % 100 == 0 and y % 400 == 0:
-    print(y, "is a leap year.")
-elif y % 400 == 0 and y % 4 == 0:
-    print(y, "is a leap year.")
-else:
-    print(y, "is not a leap year.")
